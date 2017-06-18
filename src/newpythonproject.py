f673a7dbb9c77282bcb6fa39edaa63cb6bf43022
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-06-17 22:39:12 -0400

    added error handling

--- src/newpythonproject.py
+++ src/newpythonproject.py
@@ -6,9 +6,36 @@ import gedcom
 if __name__ == "__main__":
     #print("Hello World")
     
+
     gedcomFile = gedcom.parse("Wainwright_Wroblewski.ged")
+
+    me = gedcomFile.pointers['@P1@']
+    family = gedcomFile.pointers['@F82@']
+    mom = gedcomFile.pointers['@P3@']
+   # print(me['BIRT'].place)
+   # print(family)
+    #print(mom)
     for person in gedcomFile.individuals:
-        firstname, lastname = person.name
-        print ("{0} {1} is in the file".format(firstname, lastname))
+        try:
+            birthPlaces = person['BIRT']
+            print (birthPlaces.date)
+            print(birthPlaces.place)
+        except IndexError:  # catch the error
+            pass
+        except AttributeError:
+            pass
+
+
+      #  for attr, value in stuff:
+          #  print (attr, value)
+
+
+
+    # firstname, lastname = person.name
+    # print ("{0} {1} is in the file".format(firstname, lastname))
+
+    # print(len(gedcomFile.individuals))
+
+
 
 
