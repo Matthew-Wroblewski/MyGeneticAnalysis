1064735dce5cccc4419c8ea3a015c59e0d1a585d
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-06-18 23:49:47 -0400

    fixed errors in gedcom file

--- src/newpythonproject.py
+++ src/newpythonproject.py
@@ -2,40 +2,54 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 import gedcom
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    #print("Hello World")
-    
+    # print("Hello World")
+
 
     gedcomFile = gedcom.parse("Wainwright_Wroblewski.ged")
 
     me = gedcomFile.pointers['@P1@']
-    family = gedcomFile.pointers['@F82@']
     mom = gedcomFile.pointers['@P3@']
-   # print(me['BIRT'].place)
-   # print(family)
-    #print(mom)
+    # print(me['BIRT'].place)
+    # print(family)
+    # print(mom)
+    names = []
+    birthYear = []
+    i = 0
     for person in gedcomFile.individuals:
-        try:
-            birthPlaces = person['BIRT']
-            print (birthPlaces.date)
-            print(birthPlaces.place)
-        except IndexError:  # catch the error
-            pass
-        except AttributeError:
-            pass
-
-
-      #  for attr, value in stuff:
-          #  print (attr, value)
-
-
-
+        i = i + 1
+        birthPlaces = person['BIRT']
+        firstName, lastName = person.name
+        personName = firstName + " " + lastName
+        names.append(personName)
+        birthYear.append(birthPlaces.date)
+        birthYear.append(birthPlaces.place)
+        print(names)
+        print(birthPlaces)
+        #  print(names)
+        #  print(birthYear)
+        # print(firstName, lastName)
+        ## print(birthPlaces.place)
+        #    except IndexError:  # catch the error
+        #    i = i +1
+        #    names.append(personName)
+        #    birthYear.append("Index Error")
+        #    birthYear.append("Unknown Place")
+        #    pass
+        # except AttributeError:
+        #     i = i + 1
+        #    names.append(personName)
+        #   birthYear.append("Attribute error")
+        #  birthYear.append("Unknown Dt?")
+        # pass
+    print(i)
+    print(len(names))
+    print(len(birthYear))
+    print(names)
+    print(birthYear)
     # firstname, lastname = person.name
     # print ("{0} {1} is in the file".format(firstname, lastname))
 
     # print(len(gedcomFile.individuals))
-
-
-
-
