36ed9501c4c2e25c4f093ce5fd2985aca31d7d71
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-06-19 23:23:45 -0400

    added world map and plots

--- src/newpythonproject.py
+++ src/newpythonproject.py
@@ -2,10 +2,62 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 import gedcom
+import image
+from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
+
+
 
 if __name__ == "__main__":
     # print("Hello World")
+
+    myMap = Basemap(projection="mill", llcrnrlat = 0, llcrnrlon = -120, urcrnrlat = 75, urcrnrlon = 45, resolution='l')
+    myMap.drawcoastlines()
+    myMap.drawcountries(linewidth=1)
+    myMap.drawstates(color='b')
+    myMap.fillcontinents()
+
+   # myMap.etopo()
+    myMap.bluemarble()
+    xs = []
+    ys = []
+
+    x = -79.0059
+    y = 35.7127
+    # NJ - 40.0583° N, 74.4057  --- Ukraine 48.3794° N, 31.1656° E ---
+
+    locations = ((-74.4057, 40.0853),(31.1656,49.3794))
+    print(locations[1][1])
+
+    xpt, ypt = myMap(x,y)
+
+    xs.append(x)
+    ys.append(y)
+
+    myMap.plot(xpt, ypt, 'r.', markersize=35)
+    plt.title("Birth Locations of Matt's Ancestors")
+
+    point = myMap.plot(x, y, 'ro', markersize=20)[0]
+
+
+    def init():
+        point.set_data([], [])
+        return point,
+
+    # animation function.  This is called sequentially
+    def animate(i):
+        lons, lats = locations[i]
+        x, y = myMap(lons, lats)
+        point.set_data(x, y)
+        return point,
+
+
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
+                                   frames=2, interval=2000, blit=True)
+    plt.show()
 
 
     gedcomFile = gedcom.parse("Wainwright_Wroblewski.ged")
