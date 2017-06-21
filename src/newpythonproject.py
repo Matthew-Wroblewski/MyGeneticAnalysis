76c11fe14506c3795119af74223656794fa1df47
Matthew-Wroblewski <wroblewski.m@zoho.com>
2017-06-20 23:53:49 -0400

    added coordinate checking with birth place

--- src/newpythonproject.py
+++ src/newpythonproject.py
@@ -11,53 +11,6 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    # print("Hello World")
-
-    myMap = Basemap(projection="mill", llcrnrlat = 0, llcrnrlon = -120, urcrnrlat = 75, urcrnrlon = 45, resolution='l')
-    myMap.drawcoastlines()
-    myMap.drawcountries(linewidth=1)
-    myMap.drawstates(color='b')
-    myMap.fillcontinents()
-
-   # myMap.etopo()
-    myMap.bluemarble()
-    xs = []
-    ys = []
-
-    x = -79.0059
-    y = 35.7127
-    # NJ - 40.0583° N, 74.4057  --- Ukraine 48.3794° N, 31.1656° E ---
-
-    locations = ((-74.4057, 40.0853),(31.1656,49.3794))
-    print(locations[1][1])
-
-    xpt, ypt = myMap(x,y)
-
-    xs.append(x)
-    ys.append(y)
-
-    myMap.plot(xpt, ypt, 'r.', markersize=35)
-    plt.title("Birth Locations of Matt's Ancestors")
-
-    point = myMap.plot(x, y, 'ro', markersize=20)[0]
-
-
-    def init():
-        point.set_data([], [])
-        return point,
-
-    # animation function.  This is called sequentially
-    def animate(i):
-        lons, lats = locations[i]
-        x, y = myMap(lons, lats)
-        point.set_data(x, y)
-        return point,
-
-
-    # call the animator.  blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
-                                   frames=2, interval=2000, blit=True)
-    plt.show()
 
 
     gedcomFile = gedcom.parse("Wainwright_Wroblewski.ged")
@@ -69,6 +22,7 @@ if __name__ == "__main__":
     # print(mom)
     names = []
     birthYear = []
+    birthPlace = []
     i = 0
     for person in gedcomFile.individuals:
         i = i + 1
@@ -77,13 +31,9 @@ if __name__ == "__main__":
         personName = firstName + " " + lastName
         names.append(personName)
         birthYear.append(birthPlaces.date)
-        birthYear.append(birthPlaces.place)
-        print(names)
-        print(birthPlaces)
-        #  print(names)
-        #  print(birthYear)
+        birthPlace.append(birthPlaces.place)
         # print(firstName, lastName)
-        ## print(birthPlaces.place)
+        print(birthPlace)
         #    except IndexError:  # catch the error
         #    i = i +1
         #    names.append(personName)
@@ -99,9 +49,69 @@ if __name__ == "__main__":
     print(i)
     print(len(names))
     print(len(birthYear))
+    print(len(birthPlace))
     print(names)
     print(birthYear)
-    # firstname, lastname = person.name
-    # print ("{0} {1} is in the file".format(firstname, lastname))
 
-    # print(len(gedcomFile.individuals))
+    myMap = Basemap(projection="mill", llcrnrlat=0, llcrnrlon=-120, urcrnrlat=75, urcrnrlon=45, resolution='l')
+    myMap.drawcoastlines()
+    myMap.drawcountries(linewidth=1)
+    myMap.drawstates(color='b')
+    myMap.fillcontinents()
+
+    # myMap.etopo()
+    myMap.bluemarble()
+    xs = []
+    ys = []
+
+    x = []
+    y = []
+
+    locations = ((-74.4057, 40.0853), (31.1656, 49.3794), (-74.0059, 42.0), (-61.2225, 10.6918), (-64.7505, 32.3078),
+                 (-1.4101, 53.5598))
+
+    xpt, ypt = myMap(x, y)
+
+    xs.append(x)
+    ys.append(y)
+
+    myMap.plot(xpt, ypt, 'r.', markersize=35)
+    plt.title("Birth Locations of Matt's Ancestors")
+
+    point = myMap.plot(x, y, 'ro', markersize=20)[0]
+
+
+    def init():
+        point.set_data([], [])
+        return point,
+
+    # NJ[0] -74.4057, 40.0853  --- Ukraine[1] 31.1656, 49.3794 --- New York [2] -74.0059, 42.0, Trinidad [3] -61.2225, 10.6918 --- Bermuda [4] -64.7505, 32.3078 -- England[5] -1.4101,53.5598  ----
+    #
+    # animation function.  This is called sequentially
+    def animate(i):
+        plt.text(-1.4101,53.5598,"hisafdlksajfdkl")
+        if birthPlace[i] == "England":
+            lons, lats = (-1.4101,53.5598)
+        elif birthPlace[i] == "New Jersey":
+            lons, lats = (-74.4057, 40.0853)
+        elif birthPlace[i] == "Ukraine":
+            lons, lats = (31.1656, 49.3794)
+        elif birthPlace[i] == "Trinidad":
+            lons, lats = (-61.2225, 10.6918)
+        elif birthPlace[i] == "Bermuda":
+            lons, lats = (-64.7505, 32.3078)
+        else:
+            lons, lats = (0,0)#locations[i]
+        x, y = myMap(lons, lats)
+        point.set_data(x, y)
+        return point,
+
+
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
+                                   frames=10, interval=1000, blit=True)
+    plt.show()
+    fig=plt.figure()
+    ax = fig.add_subplot(111)
+
+    print(locations[0])
